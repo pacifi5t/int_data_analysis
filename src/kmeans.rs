f40703c04b9da51a6fef20565c29dc8f99e90799
Andrii Derhach <thequasar7@gmail.com>
2022-10-18 13:11:33 +0300

    Start implementing KMeans++, add Euclid distance function

--- src/kmeans.rs
+++ src/kmeans.rs
@@ -1,39 +1,63 @@
-pub struct KMeansParameters {
+use ndarray::{Array1, Array2, Axis, ShapeError};
+use rand::{thread_rng, Rng};
+
+pub struct KMeans {
+    n_clusters: u32,
     tolerance: f64,
     max_n_iterations: u32,
 }
 
-impl KMeansParameters {
-    pub fn new(tolerance: f64, max_n_iterations: u32) -> Self {
-        KMeansParameters {
+impl Default for KMeans {
+    fn default() -> Self {
+        Self::new(3, 1e-2, 100)
+    }
+}
+
+impl KMeans {
+    pub fn new(n_clusters: u32, tolerance: f64, max_n_iterations: u32) -> Self {
+        KMeans {
+            n_clusters,
             tolerance,
             max_n_iterations,
         }
     }
 
-    pub fn tolerance(&mut self, value: f64) -> &mut KMeansParameters {
+    pub fn n_clusters(&mut self, value: u32) -> &mut Self {
+        self.n_clusters = value;
+        self
+    }
+
+    pub fn tolerance(&mut self, value: f64) -> &mut Self {
         self.tolerance = value;
         self
     }
 
-    pub fn max_n_iterations(&mut self, value: u32) -> &mut KMeansParameters {
+    pub fn max_n_iterations(&mut self, value: u32) -> &mut Self {
         self.max_n_iterations = value;
         self
     }
-}
 
-impl Default for KMeansParameters {
-    fn default() -> Self {
-        Self::new(1e-2, 100)
+    pub fn fit(&self, dataset: &Array2<f64>) {
+        // let initial_clusters = self.plus_plus_init(dataset);
+    }
+
+    fn plus_plus_init(&self, dataset: &Array2<f64>) -> Result<(), ShapeError> {
+        let mut rng = thread_rng();
+        let mut initial_centroids = Array2::zeros([0, dataset.ndim()]);
+        initial_centroids.push(Axis(0), dataset.row(rng.gen()))?;
+
+        Ok(())
+    }
+
+    fn euclid_distance(point1: Array1<f64>, point2: Array1<f64>) -> f64 {
+        let mut sum: f64 = 0.0;
+        for i in 0..point1.len() {
+            sum += (point1[i].clone() - point2[i].clone()).powi(2);
+        }
+        sum.sqrt()
     }
 }
 
-pub struct KMeans {
-    params: KMeansParameters,
-}
-
-impl KMeans {
-    pub fn new(params: KMeansParameters) -> Self {
-        KMeans { params }
-    }
+pub struct Model<T> {
+    centroids: Array2<T>,
 }
