51400c704e51329f04f06d51f0abaf0ef07fe85c
Andrii Derhach <thequasar7@gmail.com>
2022-10-20 12:29:04 +0300

    Start implementing drawing plots

--- examples/my_kmeans.rs
+++ examples/my_kmeans.rs
@@ -1,6 +1,8 @@
+use chrono::Utc;
 use csv::{ReaderBuilder, StringRecord};
 use int_data_analysis::kmeans::KMeans;
 use int_data_analysis::utils::records_into_array;
+use plotters::prelude::*;
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -13,5 +15,21 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let model = KMeans::default().n_clusters(n_clusters).fit(&data);
     println!("Result {:?}", model.centroids());
+
+    let now = Utc::now().format("(%Y-%m-%d %H:%M:%S)").to_string();
+    let filename = format!("figures/examples/my_kmeans {}.png", now);
+    let root = BitMapBackend::new(&filename, (800, 600)).into_drawing_area();
+    root.fill(&WHITE)?;
+
+    let mut chart = ChartBuilder::on(&root)
+        .caption("My KMeans", ("sans-serif", 50).into_font())
+        .margin(5)
+        .x_label_area_size(30)
+        .y_label_area_size(30)
+        .build_cartesian_2d(-30f32..30f32, -30f32..30f32)?;
+
+    chart.configure_mesh().draw()?;
+    root.present()?;
+
     Ok(())
 }
